e1e1ca514d557f99c8566dd65a80c8ccc739955c
RedaChanna <r.channa@hyntelo.com>
2024-06-21 14:21:08 +0200

    Trigger GitHub Actions

--- Tests/MyAppTests/UnitTest1.cs
+++ Tests/MyAppTests/UnitTest1.cs
@@ -8,7 +8,7 @@ public class CalculatorTests
     [InlineData(10, 2, "-", 8)]
     public void TestSimpleOperations(double num1, double num2, string operation, double expected)
     {
-        double result = Program.Calculate(num1, num2, operation); // Usa Program.Calculate
+        double result = Program.Calculate(num1, num2, operation); // Riferimento Program.Calculate
         Assert.Equal(expected, result);
     }
 }
